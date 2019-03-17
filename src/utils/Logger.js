53d64215f365210be9e0ed9fb4433b2a08bf9240
Marcus Vieira <marcus.vieira@elinvar.de>
2019-03-17 16:01:06 +0100

    Add logs path

--- src/utils/Logger.js
+++ src/utils/Logger.js
@@ -2,15 +2,6 @@ import winston from 'winston';
 require('winston-daily-rotate-file');
 const level = process.env.LOG_LEVEL || 'debug';
 
-const transportFile = new (winston.transports.DailyRotateFile)({
-  filename: 'adviqo-%DATE%.log',
-  datePattern: 'YYYY-MM-DD-HH',
-  zippedArchive: true,
-  maxSize: '20m',
-  maxFiles: '3d',
-  dirname: '../../logs' //In a real environment this value can be '/var/www/logs/'
-});
-
 const logger = new winston.Logger({
   transports: [
     new winston.transports.Console({
@@ -18,8 +9,7 @@ const logger = new winston.Logger({
       timestamp: function () {
         return (new Date()).toISOString();
       }
-    }),
-    transportFile
+    })
   ]
 });
 
